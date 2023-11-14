7b884c7f4227d18ad94d0d82dc34ea25ce82c31e
unknown <dracospy369@gmail.com>
2023-11-14 15:15:47 -0500

    s3

--- 2022/s3.py
+++ 2022/s3.py
@@ -13,6 +13,13 @@ def imp():
 if note_number > sample_number or sample_number > (note_number * (note_number + 1)) / 2:
     imp()
 
+piece_ar = [1] * note_number
+current_samples = note_number
+sample_contribution = [1] * note_number
+sample_makeup = sample_number - note_number
+
+while current_samples != sample_number:
+    piece_ar
 
 
 
@@ -39,4 +46,59 @@ Therefore, least efficent number of good samples in piece len N is N
 
 
 For piece len N, the number of good samples is:  N <= K <= (N)(N+1)/2
+
+
+
+Considering only 1s and 2s:
+
+piece | sampleN
+
+1, 1 | 2
+1, 2 | 3
+1, 2, 1 | 5
+1, 2, 2 | 4
+1, 2, 2, 1 | 5
+1, 2, 1, 2 | 7
+1, 1, 1, 1 | 4
+1, 2, 1, 1 | 6
+
+
+If 0 -> unique number and 1 -> repeated number:
+
+piece | sampleN
+
+0, 0 | 3
+1, 1 | 2
+0, 1, 1 | 4
+1, 0, 1 | 5
+1, 1, 0 | 4
+0, 1, 0, 1 | 8
+
+1, 1, 1, 1 | 4
+0, 1, 1, 1 | 5
+0, 0, 1, 1 | 7
+1, 0, 1, 1 | 6
+0, 1, 0, 1 | 7
+1, 0, 0, 1 | 9
+0, 0, 0, 0 | 10
+
+
+1, 1, 1, 1, 1 | 5
+0, 1, 1, 1, 1 | 6
+0, 0, 1, 1, 1 | 8
+0, 0, 0, 1, 1 | 11
+
+
+1, 1, 1, 1, 1, 1 | 6
+0, 1, 1, 1, 1, 1 | 7
+0, 0, 1, 1, 1, 1 | 9
+0, 0, 0, 1, 1, 1 | 12
+0, 0, 0, 0, 1, 1 | 16
+
+1, 1, 1, 1, 1, 1, 1 | 7
+0, 1, 1, 1, 1, 1, 1 | 8
+0, 0, 1, 1, 1, 1, 1 | 10
+0, 0, 0, 1, 1, 1, 1 | 13
+0, 0, 0, 0, 1, 1, 1 | 17
+0, 0, 0, 0, 0, 1, 1 | 22
 """
