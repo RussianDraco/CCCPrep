34316c62ed39180946993ee1285d17a31cb2e529
unknown <dracospy369@gmail.com>
2023-11-29 15:15:08 -0500

    s4

--- 2021/s4.py
+++ 2021/s4.py
@@ -5,35 +5,34 @@ dayNum = raw_specs[2]
 
 walkways = []
 for x in range(walkwayNum):
-    walkways.append([int(x) for x in input().split(' ')])
+    walkways.append([(int(x) - 1) for x in input().split(' ')])
 
 stationPerm = [int(x) for x in input().split(' ')]
 
 stationSwitches = []
 for x in range(dayNum):
-    stationSwitches.append([int(x) for x in input().split(' ')])
+    stationSwitches.append([(int(x) - 1) for x in input().split(' ')])
 
-def travel_time_helper(pos, trainPos, stations, curtime):
-    if stations[pos] == stationNum:
-        return curtime
-    if trainPos > stations.index(stationNum):
-        return float('inf')
+def walkwaypath(pos, time):
+    if pos == stationPerm.index(stationNum):
+        return time
 
-    actions = [[w[0]-1, w[1]-1] for w in walkways if w[0] == pos]
-    if pos == trainPos:
-        actions.append([pos, pos+1])
+    actions = [w[1] for w in walkways if w[0] == pos]
 
     if len(actions) == 0:
         return float('inf')
+    
+    return min([walkwaypath(a, time + 1) for a in actions])
 
-    return min([travel_time_helper(a[1], trainPos + 1, stations, curtime + 1) for a in actions])
+def travel_time():
+    besttime = min(
+        walkwaypath(0, float('inf')),
+        stationNum - 1
+        )
 
-
-def travel_time(stations):
-    besttime = travel_time_helper(0, 0, stations, 0)
     print(besttime)
 
 
 for x in range(dayNum):
-    stationPerm[stationSwitches[x][0] - 1], stationPerm[stationSwitches[x][1] - 1] = stationPerm[stationSwitches[x][1] - 1], stationPerm[stationSwitches[x][0] - 1]
-    travel_time(stationPerm)
+    stationPerm[stationSwitches[x][0]], stationPerm[stationSwitches[x][1]] = stationPerm[stationSwitches[x][1]], stationPerm[stationSwitches[x][0]]
+    travel_time()
