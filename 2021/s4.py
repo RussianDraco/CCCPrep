c5d5a00547f675a8f6febbe1bb4ca28eea06c9f9
unknown <dracospy369@gmail.com>
2023-11-30 14:31:53 -0500

    s4

--- 2021/s4.py
+++ 2021/s4.py
@@ -1,5 +1,5 @@
 raw_specs = [int(x) for x in input().split(' ')]
-stationNum = raw_specs[0]
+stationNum = raw_specs[0] - 1
 walkwayNum = raw_specs[1]
 dayNum = raw_specs[2]
 
@@ -7,7 +7,7 @@ walkways = []
 for x in range(walkwayNum):
     walkways.append([(int(x) - 1) for x in input().split(' ')])
 
-stationPerm = [int(x) for x in input().split(' ')]
+stationPerm = [(int(x) - 1) for x in input().split(' ')]
 
 stationSwitches = []
 for x in range(dayNum):
@@ -15,19 +15,27 @@ for x in range(dayNum):
 
 def walkwaypath(pos, time):
     if pos == stationPerm.index(stationNum):
+        print(str(pos))
+        print(str(stationPerm.index(stationNum)))
         return time
 
     actions = [w[1] for w in walkways if w[0] == pos]
 
     if len(actions) == 0:
         return float('inf')
-    
+
+    if stationPerm.index(stationNum) in actions:
+        return walkwaypath(stationPerm.index(stationNum), time + 1)
+
     return min([walkwaypath(a, time + 1) for a in actions])
+
+def combopath():
+    return min([walkwaypath(stationPerm.index(w[0]), stationPerm.index(w[0])) for w in walkways])
 
 def travel_time():
     besttime = min(
-        walkwaypath(0, float('inf')),
-        stationNum - 1
+        combopath(),
+        stationNum
         )
 
     print(besttime)
@@ -35,4 +43,5 @@ def travel_time():
 
 for x in range(dayNum):
     stationPerm[stationSwitches[x][0]], stationPerm[stationSwitches[x][1]] = stationPerm[stationSwitches[x][1]], stationPerm[stationSwitches[x][0]]
+    print(str(stationPerm))
     travel_time()
