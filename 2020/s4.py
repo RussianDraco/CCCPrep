38b1f0eecc320d9a09650d340d8f818a4b51c5f6
unknown <dracospy369@gmail.com>
2023-12-07 23:22:07 -0500

    lifesim

--- 2020/s4.py
+++ 2020/s4.py
@@ -1,5 +1,13 @@
 ppl = input()
 
-ANum = ppl.count('A')
-BNum = ppl.count('B')
-CNum = ppl.count('C')
+#Example: BABCBCACCA -> AABCBCBCCA -> AABBBCCCCA (2 swaps)
+
+ANum = ppl.count('A') #3
+BNum = ppl.count('B') #3
+CNum = ppl.count('C') #4
+
+print("BABCBCACCA"[0:3])
+
+for x in range(len(ppl)):
+    if ppl[x:x+ANum].count('A') == ANum:
+        pass
